7011425124b4ef920590ee4ad27c331c6ac1bd99
golem <golem@example.com>
2025-09-25 05:47:12 -0500

    fix(dev): set tauri default-run and clean svelte config warning

--- tauri-app/svelte.config.js
+++ tauri-app/svelte.config.js
@@ -1,8 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    cssHash: ({ hash, css }) => `s-${hash(css)}`
-  }
+  preprocess: vitePreprocess()
 };
